e4ce10b11a70c0eb283b005e0c522a5c3be114b9
jignesh.rawal <jignesh.rawal@bitbucket.aon.com>
2017-05-15 19:40:59 +0530

    Added Theme Options, UI Cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
@@ -7,7 +7,35 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-
-export class AppComponent {
-    
+export class AppComponent implements OnInit { 
+  
+ private style: string;
+  
+  ngOnInit() {
+    //Default
+   // this.changeStyle('dx.light.css');
+  }
+ /* 
+  onChange() {
+    if(this.style === 'dx.light.css')
+      this.changeStyle('dx.ios7.default.css');
+    else
+      this.changeStyle('dx.light.css');
+  }
+  
+  changeStyle(style) {
+    this.style = style;
+    var a;
+    //Change value of the meta tag
+    var links = document.querySelectorAll("link");
+    for(var i=0; i < links.length; i++) {
+        var link = links[i];
+        if(link.getAttribute("rel").indexOf("style") != -1 && link.getAttribute("title")) {
+              link.disabled = true;
+              
+        }
+        if(link.getAttribute("title") === this.style)
+                 link.disabled = false; 
+    }
+  }*/
 }
--- src/app/components/tiles/tiles.component.ts
+++ src/app/components/tiles/tiles.component.ts
@@ -1,6 +1,7 @@
 import { Component, } from '@angular/core';
 
 import { Employee, Service } from '../../app.service';
+import notify from 'devextreme/ui/notify';
 
 @Component({
     selector: 'tile-component',
@@ -9,16 +10,27 @@ import { Employee, Service } from '../../app.service';
     providers: [Service]
 })
 export class TileComponent {
+    applyButtonOptions: { text: string; type: string; onClick: (e: any, employee: any) => void; };
     currentEmployee = {};
     employees: Employee[];
     popupVisible = false;
 
     constructor(service: Service) {
         this.employees = service.getEmployees();
+
+        this.applyButtonOptions = {
+            text: "Apply",
+            type: "success",
+            onClick: (e, employee) => {
+                notify("The Apply button was clicked");
+                this.currentEmployee = employee;
+                this.popupVisible = true;
+            }
+        }
     }
 
+
     showInfo(employee) {
-        this.currentEmployee = employee;
-        this.popupVisible = true;
+
     }
 }
